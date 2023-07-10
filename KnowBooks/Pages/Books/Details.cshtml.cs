1b35a9b39ee01fc509249efdf30812565fe6603e
APandamonium1 <119054683+APandamonium1@users.noreply.github.com>
2023-07-10 13:38:44 +0800

    Part 8 - Uploading obj, Pages, Properties files

---
+++ KnowBooks/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KnowBooks.Data;
+using KnowBooks.Models;
+
+namespace KnowBooks.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly KnowBooks.Data.KnowBooksContext _context;
+
+        public DetailsModel(KnowBooks.Data.KnowBooksContext context)
+        {
+            _context = context;
+        }
+
+      public Book Book { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ISBN == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
---
+++ KnowBooks/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KnowBooks.Data;
+using KnowBooks.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace KnowBooks.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KnowBooks.Data.KnowBooksContext _context;
+
+        public IndexModel(KnowBooks.Data.KnowBooksContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? BookGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from b in _context.Book
+                                            orderby b.Genre
+                                            select b.Genre;
+            var books = from b in _context.Book
+                         select b;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(BookGenre))
+            {
+                books = books.Where(x => x.Genre == BookGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Book = await books.ToListAsync();
+        }
+    }
+}
