688fbd4c1917efccc500384311e46869d11745bb
robinchang1 <s10242331@connect.np.edu.sg>
2023-07-20 21:05:05 +0800

    added email verification

--- KnowBooks/Program.cs
+++ KnowBooks/Program.cs
@@ -3,67 +3,106 @@ using Microsoft.Extensions.DependencyInjection;
 using KnowBooks.Data;
 using KnowBooks.Models;
 using Microsoft.AspNetCore.Identity;
-var builder = WebApplication.CreateBuilder(args);
+using KnowBooks.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
-// Add services to the container.
-builder.Services.AddRazorPages(options =>
+namespace KnowBooks
 {
-    //options.Conventions.AuthorizePage("/Books/Create");
-    //options.Conventions.AuthorizeAreaPage("Identity", "/Account/Manage");
-    options.Conventions.AuthorizeFolder("/Books");
-});
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<KnowBooksContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("KnowBooksContext") ?? throw new InvalidOperationException("Connection string 'KnowBooksContext' not found.")));
+            // Add services to the container.
+            builder.Services.AddRazorPages(options =>
+            {
+                //options.Conventions.AuthorizePage("/Books/Create");
+                //options.Conventions.AuthorizeAreaPage("Identity", "/Account/Manage");
+                options.Conventions.AuthorizeFolder("/Books");
+            });
+
+            builder.Services.AddDbContext<KnowBooksContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("KnowBooksContext") ?? throw new InvalidOperationException("Connection string 'KnowBooksContext' not found.")));
 
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddRoles<IdentityRole>().AddEntityFrameworkStores<KnowBooksContext>();
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<KnowBooksContext>();
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 5;
-    options.Password.RequiredUniqueChars = 1;
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 10;
-    options.Lockout.AllowedForNewUsers = true;
-    // User settings.
-    options.User.AllowedUserNameCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
-});
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 1;
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
 
-var app = builder.Build();
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
+            builder.Services.Configure<EmailSender>(builder.Configuration);
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
+            var app = builder.Build();
 
-    SeedData.Initialize(services);
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Program>();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                // ... Other services
+
+                // Register your email sender implementation
+                services.AddSingleton<IEmailSender, EmailSender>();
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    // ... Other options
+                })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<KnowBooksContext>();
+            });
+    }
 }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseAuthentication();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
---
+++ KnowBooks/Services/EmailSender.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+
+
+namespace KnowBooks.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+            var smtpServer = emailSettings["SMTPServer"];
+            var smtpPort = int.Parse(emailSettings["SMTPPort"]);
+            var smtpUserName = emailSettings["SMTPUserName"];
+            var smtpPassword = emailSettings["SMTPPassword"];
+            var senderEmail = emailSettings["SenderEmail"];
+            var senderName = emailSettings["SenderName"];
+
+            using (var smtpClient = new SmtpClient(smtpServer, smtpPort))
+            {
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(smtpUserName, smtpPassword);
+                smtpClient.EnableSsl = true;
+
+                using (var mailMessage = new MailMessage())
+                {
+                    mailMessage.From = new MailAddress(senderEmail, senderName);
+                    mailMessage.To.Add(new MailAddress(email));
+                    mailMessage.Subject = subject;
+                    mailMessage.Body = message;
+                    mailMessage.IsBodyHtml = true;
+
+                    smtpClient.Send(mailMessage);
+
+                    //await smtpClient.SendMailAsync(mailMessage);
+                }
+            }
+        }
+    }
+}
