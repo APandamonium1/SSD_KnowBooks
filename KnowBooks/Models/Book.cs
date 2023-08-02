57d4400cf2db1357b0aa32de9417f116393dee34
PandaJW <5168pandayoyo@gmail.com>
2023-08-02 15:01:35 +0800

    Redited Availability status

--- KnowBooks/Models/Book.cs
+++ KnowBooks/Models/Book.cs
@@ -26,20 +26,13 @@ namespace KnowBooks.Models
         //Indicates Genre as mandatory field
         public string Genre { get; set; } = string.Empty;
 
-        /*
-        [RegularExpression("^[A-Za-z]{1,20}$", ErrorMessage = "Please enter either 'Available' or 'Loaned' only.")]
-        private string? _availabilityStatus;
+
+        //[RegularExpression("^[A-Za-z]{1,20}$", ErrorMessage = "Please enter either 'Available' or 'Loaned' only.")] //RegEx only, capitalise 1st letter
+        [Required, StringLength(9, MinimumLength =6, ErrorMessage = "Please enter either 'Available' or 'Loaned' only.")]
         [Display(Name = "Available Status")]//Displays as 2 words instead of AvailabilityStatus
         //Following code to restrict the input for Availability Status to only 2 string values ("Available" or "Loaned")
-        public string AvailabilityStatus
-        {
-            get { return _availabilityStatus; }
-            set 
-            {
-                // Set the default value of the AvailabilityStatus property to Available
-                //_availabilityStatus = "Available";
-            }
-        }*/
+        [RegularExpression("^[A-Za-z]{1,20}$", ErrorMessage ="Please enter either 'Available' or 'Loaned' only.")]
+        public string AvailabilityStatus {get; set; }
 
         public string AvailabilityStatus { get; set; }
 
