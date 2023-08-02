5625e3e8c83e516e9a30ccc78e5071c47eb3714f
robinchang1 <s10242331@connect.np.edu.sg>
2023-08-02 19:04:34 +0800

    .

--- KnowBooks/Models/Review.cs
+++ KnowBooks/Models/Review.cs
@@ -1,6 +1,18 @@
-﻿namespace KnowBooks.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KnowBooks.Models
 {
     public class Review
     {
+        [Key] //Sets ISBN as primary key with PK constraints
+        public int ISBN { get; set; }
+
+        public string BookTitle { get; set; } = string.Empty;
+
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int Rating { get; set; }
+
+        [StringLength(100, MinimumLength = 1)]
+        public string Text { get; set; } = string.Empty;
     }
 }
