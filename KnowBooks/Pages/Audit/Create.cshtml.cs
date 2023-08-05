c583d16ab4441980de023632fd1e47a168a2627d
PandaJW <5168pandayoyo@gmail.com>
2023-08-05 18:37:27 +0800

    Added Audit pages

---
+++ KnowBooks/Pages/Audit/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using KnowBooks.Data;
+using RazorPagesMovie.Models;
+
+namespace KnowBooks.Pages.Audit
+{
+    public class CreateModel : PageModel
+    {
+        private readonly KnowBooks.Data.KnowBooksContext _context;
+
+        public CreateModel(KnowBooks.Data.KnowBooksContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public AuditRecord AuditRecord { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.AuditRecords == null || AuditRecord == null)
+            {
+                return Page();
+            }
+
+            _context.AuditRecords.Add(AuditRecord);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ KnowBooks/Pages/Audit/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KnowBooks.Data;
+using RazorPagesMovie.Models;
+
+namespace KnowBooks.Pages.Audit
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KnowBooks.Data.KnowBooksContext _context;
+
+        public IndexModel(KnowBooks.Data.KnowBooksContext context)
+        {
+            _context = context;
+        }
+
+        public IList<AuditRecord> AuditRecord { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.AuditRecords != null)
+            {
+                AuditRecord = await _context.AuditRecords.ToListAsync();
+            }
+        }
+    }
+}
