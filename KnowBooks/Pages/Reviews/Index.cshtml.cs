4f372976643799d12998e4fd2b9d046781833559
robinchang1 <s10242331@connect.np.edu.sg>
2023-08-06 12:06:28 +0800

    Added Filtering for Reviews

--- KnowBooks/Pages/Reviews/Index.cshtml.cs
+++ KnowBooks/Pages/Reviews/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using KnowBooks.Data;
 using KnowBooks.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.IdentityModel.Tokens;
 
 namespace KnowBooks.Pages.Reviews
 {
@@ -21,12 +23,38 @@ namespace KnowBooks.Pages.Reviews
 
         public IList<Review> Review { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public int Rating { get; set; }
+
+        public SelectList? Titles { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? BookTitle { get; set; }
+
         public async Task OnGetAsync()
         {
             if (_context.Review != null)
             {
                 Review = await _context.Review.ToListAsync();
             }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from b in _context.Review
+                                            orderby b.BookTitle
+                                            select b.BookTitle;
+            var reviews = from b in _context.Review
+                        select b;
+            if (Rating != 0)
+            {
+                reviews = reviews.Where(s => s.Rating == Rating);
+            }
+
+            if (!string.IsNullOrEmpty(BookTitle))
+            {
+                reviews = reviews.Where(x => x.BookTitle == BookTitle);
+            }
+            Titles = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Review = await reviews.ToListAsync();
         }
     }
 }
